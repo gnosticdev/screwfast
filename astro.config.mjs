a8b5760148de70a37a28818f6f9093fdd35bf963
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-04-04 22:45:05 +0400

    feat: Create separate branch for single language version

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
   image: {
     domains: ["images.unsplash.com"],
   },
+<<<<<<< HEAD
   // i18n: {
   //   defaultLocale: "en",
   //   locales: ["en", "fr"],
@@ -22,18 +23,12 @@ export default defineConfig({
   //     prefixDefaultLocale: false,
   //   },
   // },
+=======
+>>>>>>> ff3f4fa (feat: Create separate branch for single language version)
   prefetch: true,
   integrations: [
     tailwind(),
-    sitemap({
-      i18n: {
-        defaultLocale: "en", // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
-        locales: {
-          en: "en", // The `defaultLocale` value must present in `locales` keys
-          fr: "fr",
-        },
-      },
-    }),
+    sitemap(),
     starlight({
       title: "ScrewFast Docs",
       defaultLocale: "root",
