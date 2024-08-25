8b8dc9a6e0972b0e93bef676e3ce0233298fcf37
gnostic <64601257+nynxa@users.noreply.github.com>
2024-08-25 12:21:34 -0400

    feat: fork customizations: remove vercel, add _headers for cloudflare, replace prettier with @biomejs/biome. replace globby and node for process-html.mjs Bun. switch main branch to monolingual (english) site.

--- process-html.mjs
+++ process-html.mjs
@@ -1,23 +1,21 @@
-import fs from 'node:fs/promises'
-import { globby } from 'globby'
 import { minify } from 'html-minifier-terser'
 
 // Get all HTML files from the output directory
-const path = './.vercel/output/static'
-const files = await globby(`${path}/**/*.html`)
+const path = 'dist'
+const glob = new Bun.Glob('**/*.html')
+const files = await Array.fromAsync(glob.scan({ cwd: path }))
+console.log(files)
+for (const file of files) {
+    let html = await Bun.file(`${path}/${file}`).text()
 
-await Promise.all(
-    files.map(async (file) => {
-   	 console.log('Processing file:', file)
-   	 let html = await fs.readFile(file, 'utf-8')
-
-   	 // Minify the HTML
-   	 html = await minify(html, {
-   		 removeComments: true,
-   		 preserveLineBreaks: true,
-   		 collapseWhitespace: true,
-		 minifyJS: true
-   	 })
-   	 await fs.writeFile(file, html)
+    // Minify the HTML
+    html = await minify(html, {
+        removeComments: true,
+        preserveLineBreaks: true,
+        collapseWhitespace: true,
+        minifyJS: true,
     })
-)
+    await Bun.write(`${path}/${file}`, html)
+}
+
+console.log('finished minifying html files')
